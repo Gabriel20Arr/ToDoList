18991f3fce99fc49e47eab3a2e9ea59d6792dbe5
Gabriel <gabriel2021arr@gmail.com>
2024-02-21 15:00:51 -0300

    cambios

--- api/src/config.js
+++ api/src/config.js
@@ -1,3 +1,3 @@
 export const TOKEN_SECRET = "token key";
 export const URL_LOCALHOST2 = process.env.URL_LOCALHOST || 'http://localhost:5173';
-export const PORT = process.evn || 4000;
+export const PORT = process.evn.PORT || 4000;
