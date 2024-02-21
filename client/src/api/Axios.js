068c7b1a96c3f068f23b75ddc38b10cb69febbe0
Gabriel <gabriel2021arr@gmail.com>
2024-02-21 14:13:37 -0300

    despliegue front

--- client/src/api/Axios.js
+++ client/src/api/Axios.js
@@ -4,7 +4,6 @@ const URLBACK = import.meta.env.VITE_LOCALHOSTBACK || "http://localhost:4000";
  
 const instance = axios.create({
     baseURL: `${URLBACK}/api`,
-    // baseURL: 'https://to-do-list-3z3d.vercel.app/api',
     withCredentials: true
 })
 
