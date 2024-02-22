9acbf22c44179e5008b2695712197c46d8d29207
Gabriel <gabriel2021arr@gmail.com>
2024-02-22 11:48:17 -0300

    controller config 5

--- api/src/Controllers/auth.controllers.js
+++ api/src/Controllers/auth.controllers.js
@@ -57,8 +57,7 @@ export const login = async (req, res) =>  {
 
         const token = await createAccessToken({id: userFound._id})
 
-    res
-    .cookie("token", token)   
+    res.cookie("token", token)   
 
     res.json({
         id: userFound._id,
