e606e1d300f9b8eefd20947aae4bc776d887462d
Gabriel <gabriel2021arr@gmail.com>
2024-02-23 09:09:36 -0300

    ajustes front deploy

--- client/src/api/Axios.js
+++ client/src/api/Axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const URLBACK = "http://localhost:4000" || import.meta.env.VITE_LOCALHOSTBACK 
+// const URLBACK = "http://localhost:4000" || import.meta.env.VITE_LOCALHOSTBACK 
+const URLBACK = "http://localhost:4000" || "https://backendtodo-q512.onrender.com/api"
  
 const instance = axios.create({
     baseURL: "https://backendtodo-q512.onrender.com/api",
