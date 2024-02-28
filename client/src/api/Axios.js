885a3eab60aa51db8f10bc98997a977a417c07a7
Gabriel <gabriel2021arr@gmail.com>
2024-02-28 11:22:41 -0300

    ultimas configuraciones

--- client/src/api/Axios.js
+++ client/src/api/Axios.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-// const URLBACK = "http://localhost:4000" || import.meta.env.VITE_LOCALHOSTBACK 
-const URLBACK = "http://localhost:4000" || "https://backendtodo-q512.onrender.com/api"
+// const URLBACK = import.meta.env.VITE_LOCALHOSTBACK 
+const URLBACK = "http://localhost:4000"
  
 const instance = axios.create({
-    baseURL: "https://backendtodo-q512.onrender.com/api",
-    // baseURL: `${URLBACK}/api`,
+    baseURL: `${URLBACK}/api`,
     withCredentials: true
 })
 
