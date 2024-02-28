ba9797e2e4ed810f5667f03ce8391883101eed00
Gabriel <gabriel2021arr@gmail.com>
2024-02-28 11:29:41 -0300

    ultimas configuraciones2

--- client/src/api/Axios.js
+++ client/src/api/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const URLBACK = import.meta.env.VITE_LOCALHOSTBACK 
-const URLBACK = "http://localhost:4000"
+const URLBACK = import.meta.env.VITE_LOCALHOSTBACK 
+// const URLBACK = "http://localhost:4000"
  
 const instance = axios.create({
     baseURL: `${URLBACK}/api`,
